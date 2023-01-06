64f9d8cc1b5f5b87b6a287799db58958ca34d9f4
javgarmon <javier.garces.m@gmail.com>
2023-01-06 10:43:29 +0000

    first version

--- src/app.js
+++ src/app.js
@@ -6,6 +6,53 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //Declaración de funciones para generar palos y números aleatorios
+  function randomPalo() {
+    const palos = ["♦", "♥", "♠", "♣"];
+    let indexPalo = Math.floor(Math.random() * palos.length);
+    return palos[indexPalo];
+  }
+
+  function randomNumber() {
+    const number = ["A", "2", "3", "4", "5", "6", "8", "9", "J", "Q", "K"];
+    let indexNumber = Math.floor(Math.random() * number.length);
+    return number[indexNumber];
+  }
+
+  //Gardamos las funciones en un espacio de memoria
+  let generarPalo = randomPalo();
+  let generarNumero = randomNumber();
+
+  //Selección de elementos del html
+  let top = document.querySelector(".top");
+  let bottom = document.querySelector(".bottom");
+  let numero = document.querySelector(".numbers");
+
+  //Incrustar las variables en el html (imprimir)
+  top.innerHTML = generarPalo;
+  bottom.innerHTML = generarPalo;
+  numero.innerHTML = generarNumero;
+
+  //Codicionales para asignar color a los palos
+  if (top.innerHTML === "♦" || top.innerHTML === "♥") {
+    top.style.color = "red";
+  } else {
+    top.style.color = "black";
+  }
+
+  if (bottom.innerHTML === "♦" || bottom.innerHTML === "♥") {
+    bottom.style.color = "red";
+  } else {
+    bottom.style.color = "black";
+  }
 };
+
+console.log("Hello Rigo from the console!");
+
+//Dar funcionalidad al botón "Change card"
+// let changeCard = document.getElementById("btn1");
+// changeCard.addEventListener("click", _ => {
+//   location.reload;
+// });
+
+//CountDown Timer
